0d5197f4397c3fed6be2dca2fb6fb09a1a15e5ce
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-07-28 13:17:43 +0900

    update

--- main.ts
+++ main.ts
@@ -157,6 +157,7 @@ namespace rtc {
     export function setClock(year: number, month: number, day: number, weekday: number, hour: number, minute: number, second: number): void {
         let buf = pins.createBuffer(8);
 
+        if (deviceType == 5) buf[0] = REG_SECOND << 4 || 0; else buf[0] = REG_SECOND;
         buf[REG_SECOND + 1] = DecToHex(second);
         buf[REG_MINUTE + 1] = DecToHex(minute);
         switch (deviceType) {
